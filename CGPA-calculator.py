4f4ce2fc6ae7a5adfbb1c72b008068c7caf19ae8
unknown <chaudharyadeel688@gmail.com>
2024-09-09 11:34:39 +0500

    initial commit

---
+++ CGPA-calculator.py
@@ -0,0 +1,72 @@
+#1st project of making a grade and cgpa calculator
+marks = int(input("Enter the marks: "))
+if marks >= 95:
+    print("Your CGPA is 4.0 and grade is A+")
+elif marks >= 85 and marks < 95:
+    print("Your CGPA is 4.0 and grade is A")
+elif marks >= 83 and marks < 85:
+    print("Your CGPA is 3.9 and grade is B+")
+elif marks == 82:
+    print("Your CGPA is 3.8 and grade is B+")
+elif marks >= 80 and marks < 82:
+    print("Your CGPA is 3.7 and grade is B+")
+elif marks == 79:
+    print("Your CGPA is 3.6 and grade is B")
+elif marks >= 77 and marks < 79:
+    print("Your CGPA is 3.5 and grade is B")
+elif marks == 76:
+    print("Your CGPA is 3.4 and grade is B")
+elif marks >= 74 and marks < 76:
+    print("Your CGPA is 3.3 and grade is B")
+elif marks == 73:
+    print("Your CGPA is 3.2 and grade is B")
+elif marks >= 71 and marks < 73:
+    print("Your CGPA is 3.1 and grade is B")
+elif marks == 70:
+    print("Your CGPA is 3.0 and grade is B")
+elif marks == 69:
+    print("Your CGPA is 2.9 and grade is C")
+elif marks == 68:
+    print("Your CGPA is 2.8 and grade is C")
+elif marks == 67:
+    print("Your CGPA is 2.7 and grade is C")
+elif marks == 66:
+    print("Your CGPA is 2.6 and grade is C")
+elif marks == 65:
+    print("Your CGPA is 2.5 and grade is C")
+elif marks == 64:
+    print("Your CGPA is 2.4 and grade is C")
+elif marks == 63:
+    print("Your CGPA is 2.3 and grade is C")
+elif marks == 62:
+    print("Your CGPA is 2.2 and grade is C")
+elif marks == 61:
+    print("Your CGPA is 2.1 and grade is C")
+elif marks == 60:
+    print("Your CGPA is 2.0 and grade is C")
+elif marks == 59:
+    print("Your CGPA is 1.9 and grade is D")
+elif marks == 58:
+    print("Your CGPA is 1.8 and grade is D")
+elif marks == 57:
+    print("Your CGPA is 1.7 and grade is D")
+elif marks == 56:
+    print("Your CGPA is 1.6 and grade is D")
+elif marks == 55:
+    print("Your CGPA is 1.5 and grade is D")
+elif marks == 54:
+    print("Your CGPA is 1.4 and grade is D")
+elif marks == 53:
+    print("Your CGPA is 1.3 and grade is D")
+elif marks == 52:
+    print("Your CGPA is 1.2 and grade is D")
+elif marks == 51:
+    print("Your CGPA is 1.1 and grade is D")
+elif marks == 50:
+    print("Your CGPA is 1.0 and grade is D")
+else :
+    print("You are fail")
+
+
+
+
--- home/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Student(models.Model):
-    name = models.CharField(max_length=250)
-    rollno = models.CharField(max_length=250)
-    department = models.CharField(max_length=250)
-    email = models.EmailField(max_length=250)
